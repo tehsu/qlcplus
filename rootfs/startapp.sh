86997ac45a8fff5ad99609ec8c2b77c146455211
Tehseen <tehsu@snyde.net>
2024-09-16 20:01:46 -0500

    try

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -3,9 +3,9 @@
 set -u # Treat unset variables as an error.
 
 start_qlc() {
-	/usr/bin/qlcplus -o -w /QLC/qlc.qxw -p
+	/usr/bin/qlcplus -m -w -p
 }
-
+export QT_QPA_PLATFORM=offscreen
 log_debug "starting QLC+..."
 start_qlc
 
